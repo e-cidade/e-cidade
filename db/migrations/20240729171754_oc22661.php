45cc8818d4ea7be2c7a8fe34b3ed6aab4dfbf662
Igor Ruas <igor@contassconsultoria.com.br>
2024-07-31 15:57:14 -0300

    altera local de arquivos

---
+++ db/migrations/20240729171754_oc22661.php
@@ -0,0 +1,28 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Oc22661 extends AbstractMigration
+{
+
+    public function up()
+    {
+//        $sSql = "
+//        BEGIN;
+//
+//        delete from itemprecoreferencia where si02_itemproccompra in (select pc22_orcamitem from itemprecoreferencia
+//        left join pcorcamitem on pc22_orcamitem = si02_itemproccompra where pc22_orcamitem is null);
+//
+//        ALTER TABLE precoreferencia ADD CONSTRAINT precoreferencia_cgmcotacao_fk FOREIGN KEY(si01_numcgmcotacao) REFERENCES cgm(z01_numcgm);
+//        ALTER TABLE precoreferencia ADD CONSTRAINT precoreferencia_cgmorcamento_fk FOREIGN KEY(si01_numcgmorcamento) REFERENCES cgm(z01_numcgm);
+//
+//        ALTER TABLE itemprecoreferencia ADD CONSTRAINT itemprecoreferencia_itemproccompra_fk FOREIGN KEY(si02_itemproccompra) REFERENCES pcorcamitem(pc22_orcamitem);
+//        ALTER TABLE itemprecoreferencia ADD CONSTRAINT itemprecoreferencia_coditem_fk FOREIGN KEY(si02_coditem) REFERENCES pcmater(pc01_codmater);
+//        ALTER TABLE itemprecoreferencia ADD CONSTRAINT itemprecoreferencia_codunidadeitem_fk FOREIGN KEY(si02_codunidadeitem) REFERENCES matunid(m61_codmatunid);
+//
+//        COMMIT;
+//        ";
+//
+//        $this->execute($sSql);
+    }
+}
